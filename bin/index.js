5fcf90cd1ce3f71184ed7d95a38e89757dd9167f
徐兴 <xuxing@hynn.com.cn>
2024-05-22 18:05:16 +0800

    feat: 脚手架增加重复文件检查与强制覆盖功能

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,7 @@ const program = new Command();
 program
   .command("create <app-name>") // 创建命令
   .description("create a new project") // 命令描述
+  .option("-f, --force", "overwrite target directory if it is existed") // 命令选项(选项名，描述) 这里解决下重名的情况
   .action((name, options, cmd) => {
     console.log("执行 create 命令");
 
@@ -20,14 +21,16 @@ program
 program.on("--help", () => {
   console.log();
   console.log(
-    `Run ${chalk.cyan("rippi <command> --help")} to show detail of this command`
+    `Run ${chalk.cyan(
+      "simple <command> --help"
+    )} to show detail of this command`
   );
   console.log();
 });
 
 program
   // 说明版本
-  .version(`rippi-cli@${config.version}`)
+  .version(`simple-cli@${config.version}`)
   // 说明使用方式
   .usage("<command [option]");
 
